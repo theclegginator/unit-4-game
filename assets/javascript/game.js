f47ea56fe30d33e2569597a4d2194d445e2e1c97
GekidouX <47258390+theclegginator@users.noreply.github.com>
2019-03-21 01:01:06 -0400

    switched over to ruby game. Got everything working. Still needs some styling.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,79 +1,72 @@
-// 1. Initialize the stats of all characters by making them an object. Attack button should be disabled at the beginning of the game.
-var player = undefined;
-var char1 = {
-    HP: 100,
-    baseAttack: 10,
-    currentAttack: 10,
-    counterAttack: 5
-}
-var char2 = {
-    HP: 135,
-    baseAttack: 8,
-    currentAttack: 10,
-    counterAttack: 5
-}
-var char3 = {
-    HP: 180,
-    baseAttack: 6,
-    currentAttack: 10,
-    counterAttack: 5
-}
-var char4 = {
-    HP: 142,
-    baseAttack: 9,
-    currentAttack: 10,
-    counterAttack: 5
-}
+// initialize variables
+var wins = 0;
+var losses = 0;
+var answer = 0;
+var crystal1 = 0;
+var crystal2 = 0;
+var crystal3 = 0;
+var crystal3 = 0;
+var score = 0;
 
-// Player selects a character by clicking on one of the four images positioned at the top. Use jQuery on click events. These characters will be in a container at the top of the screen.
-if (player === undefined) {
-    $("#char1").on("click", function() {
-        player = char1;
-        $("#char1").appendTo("#battleArena");
-        $("#char2").appendTo("#enemySelect");
-        $("#char3").appendTo("#enemySelect");
-        $("#char4").appendTo("#enemySelect");
-    })
-    $("#char2").on("click", function() {
-        player = char2;
-        $("#char1").appendTo("#enemySelect");
-        $("#char3").appendTo("#enemySelect");
-        $("#char4").appendTo("#enemySelect");
-    })
-    $("#char3").on("click", function() {
-        player = char3;
-        $("#char1").appendTo("#enemySelect");
-        $("#char2").appendTo("#enemySelect");
-        $("#char4").appendTo("#enemySelect");
-    })
-    $("#char4").on("click", function() {
-        player = char4;
-        $("#char1").appendTo("#enemySelect");
-        $("#char2").appendTo("#enemySelect");
-        $("#char3").appendTo("#enemySelect");
-    })
+// gameStart resets the random number variables and updates wins/losses in the DOM
+function gameStart() {
+    // generate a random number that the player has to guess.
+    answer = 19 + Math.ceil(101*Math.random());
+    $("#answer").text(answer); 
+    // generate four random numbers for each of the crystals.
+    crystal1 = 1 + Math.ceil(11*Math.random())
+    crystal2 = 1 + Math.ceil(11*Math.random())
+    crystal3 = 1 + Math.ceil(11*Math.random())
+    crystal4 = 1 + Math.ceil(11*Math.random())
+    console.log(crystal1, crystal2, crystal3, crystal4)
+    // reset player score.
+    score = 0;
+    $("#score").text(score);
+    $("#wins").text(wins);
+    $("#losses").text(losses);
 }
 
+// checkScore checks the win/loss conditions after each guess and starts new rounds.
+function checkScore() {
+    // If player score = answer, wins++; gameStart()
+    if (score === answer) {
+        wins++;
+        gameStart();
+    }
+    // If player score > answer, losses++; gameStart()
+    else if (score > answer) {
+        losses++;
+        gameStart();
+    }
+    // If player score < answer, keep playing
+}
 
+gameStart (); // start the first instance of the game.
 
-// Move the player character to the battle arena part of the screen
+// click events assigned to each image id that will increase number value by a certain amount
+// after each click, call the checkScore function to determine game results.
 
-// The player then selects an opponent by clicking one of the available enemy characters. 
-// var opponent = char2, etc
-
-// After selecting a character to fight, they are moved to the battle arena part of the screen
-
-// Once an opponent is selected, the "Attack" button will become active instead of disabled.
-
-// ===== BATTLE SECTION =====
-
-    // Decrease Opponent's HP by player's currentAttack power
-
-        // Special case to ensure HP = 0 if the HP value goes negative.
-
-    // Decrease Player's HP by opponent's counterAttack power
-
-        // Special case to ensure HP = 0 if the HP value goes negative.
-
-    // Each time player attacks, their attack power increases by their base attack power.
-    //player.currentAttack += player.baseAttack;
+// Crystal Type 1 - Ruby
+$("#crystal1").click(function() {
+    score = score + crystal1;
+    $("#score").html(score);
+    checkScore();
+})
+// Crystal Type 2 - Saphire
+$("#crystal2").click(function() {
+    score += crystal2;
+    $("#score").text(score);
+    checkScore();
+})
+// Crystal Type 3 - Emerald
+$("#crystal3").click(function() {
+    score += crystal3;
+    $("#score").text(score);
+    checkScore();
+})
+// Crystal Type 4 - Garnet
+$("#crystal4").click(function() {
+    score += crystal4;
+    $("#score").text(score);
+    checkScore();
+})
